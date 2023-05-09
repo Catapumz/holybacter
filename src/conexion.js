1cb7a0cfcf49c4f0206a1b7e74a572da50f242ca
Carlos Cataluña Ronda <carloscataronda@gmail.com>
2023-05-09 20:16:10 +0200

    First commit

--- src/conexion.js
+++ src/conexion.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 const conexion = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/holybacter");
-  //await mongoose.connect("mongodb://mongo:3010/holybacter"); // esto para amazon
+  // await mongoose.connect("mongodb://127.0.0.1:27017/holybacter");
+  await mongoose.connect("mongodb://mongo:3010/holybacter"); // esto para amazon
   console.log("Conectado correctamente a la base de datos de holybacter");
 };
 
